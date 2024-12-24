bc769277c1825baad7f997c7e20b059120622074
karimtmagdy <karimtmagdy@gmail.com>
2024-12-24 07:57:26 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -46,15 +46,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => nav("/admin")}>admin</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
