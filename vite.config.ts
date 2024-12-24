cf4717536d0fcf3a4e06af961d02cb5172b1df84
karimtmagdy <karimtmagdy@gmail.com>
2024-12-24 08:31:56 +0200

    update port

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   root: ".",
   base: "./",
+  server: {
+    port: 3000,
+  },
   plugins: [react()],
   resolve: {
     alias: {
